a386ec9b78b80eb96a123611c676f790f3daa62f
Irupc <68991705+Irupc@users.noreply.github.com>
2021-08-18 21:23:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ async def inline(bot, update):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [InlineKeyboardButton(text="Open Link", url=result["link"])],
-                        [InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)],
+                        [InlineKeyboardButton('Search again', switch_inline_query_current_chat="")],
                     ],[
                         [InlineKeyboardButton(text="For More TG Bots", url="https://t.me/tgbotslk")],
                         JOIN_BUTTON
